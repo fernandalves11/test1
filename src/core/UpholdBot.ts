d0cf4428cfac4ce55a9a2b83a3ea263baa79b940
fernandalves11 <falves.nani@proton.me>
2025-10-28 16:45:26 +0000

    test minor imporvs

--- src/core/UpholdBot.ts
+++ src/core/UpholdBot.ts
@@ -9,35 +9,14 @@ import { Logger } from '../utils/Logger.js';
  */
 export class UpholdBot {
   private botConfig: BotConfig;
-  private priceFetcherServ: PriceService;
+  private priceServ: PriceService;
   private alertServ: AlertService;
-  private lastPrices: Map<string, number> = new Map();
   private intervalId: ReturnType<typeof setInterval> | null = null;
 
   constructor() {
     this.botConfig = new BotConfig();
-    this.priceFetcherServ = new PriceService();
+    this.priceServ = new PriceService();
     this.alertServ = new AlertService();
-  }
-
-  /**
-   * Checks whether the current price differs from the previous price by more than the configured threshold. 
-   * If yes, triggers an alert.
-   */
-  private checkOscillation(pair: string, currentPrice: number): void {
-    // store last price from the current pair for the next ticker update
-    const previous = this.lastPrices.get(pair);
-    this.lastPrices.set(pair, currentPrice);
-
-    //skips comparison if is the first recorded price
-    if (!previous) return;
-
-    const change = Math.abs((currentPrice - previous) / previous) * 100;
-
-    //if price changed more than threshold, fire alert
-    if (change >= this.botConfig.getThreshold()) {
-      this.alertServ.triggerAlert(pair, change, previous, currentPrice);
-    }
   }
 
   /**
@@ -47,14 +26,24 @@ export class UpholdBot {
     Logger.info("▶️  Starting Uphold price alert bot. Time: ", new Date().toLocaleTimeString());
     Logger.info("🛠️  Bot Config Options: ", JSON.stringify(this.botConfig.toJSON(), null, 2));
 
+
     //schedule price checks using the configured interval
     this.intervalId = setInterval(async () => {
       for (const pair of this.botConfig.getPairs()) {
         //gets price from API through price service
-        const price = await this.priceFetcherServ.getPrice(pair);
-        
-        //check for oscilation in the retrieved price
-        if (price !== null) this.checkOscillation(pair, price);
+        const currentPrice = await this.priceServ.getPriceExternalAPI(pair);
+
+        // store last price from the current pair for the next ticker update
+        const previous = this.priceServ.getLastPrice(pair);
+        this.priceServ.setLastPrice(pair, currentPrice);
+
+        //if it's the first time, there is no change to compare
+        if (!previous) return;
+
+        const change = this.priceServ.getPriceChangePercentage(previous, currentPrice);
+        if (change >= this.botConfig.getThreshold()) {
+          this.alertServ.triggerAlert(pair, change, previous, currentPrice);
+        }
       }
     }, this.botConfig.getInterval());
 
